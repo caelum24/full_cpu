a8c97db2b95b1143aa6f2b4e27e6048fac528bb1
Nico Molina <nicomolina2002@gmail.com>
2024-04-03 17:31:38 -0400

    added test files

---
+++ Test Files/Assembly Files/fpga_test.s
@@ -0,0 +1,41 @@
+nop     # Author: Molina, Nico
+nop
+nop
+nop
+nop
+nop 
+addi $r0, $r0, 0
+addi $r1, $r1, 1
+addi $r2, $r2, 3
+addi $r3, $r3, 2
+addi $r4, $r4, 5
+addi $r5, $r5, 4
+addi $r6, $r6, 7
+addi $r7, $r7, 6
+addi $r8, $r8, 9
+addi $r9, $r9, 8
+addi $r10, $r10, 11
+addi $r11, $r11, 10
+addi $r12, $r12, 13
+addi $r13, $r13, 12
+addi $r14, $r14, 15
+addi $r15, $r15, 14
+addi $r16, $r16, 17
+addi $r17, $r17, 16
+addi $r18, $r18, 19
+addi $r19, $r19, 18
+addi $r20, $r20, 21
+addi $r21, $r21, 20
+addi $r22, $r22, 23
+addi $r23, $r23, 22
+addi $r24, $r24, 25
+addi $r25, $r25, 24
+addi $r26, $r26, 27
+addi $r27, $r27, 26
+addi $r28, $r28, 29
+addi $r29, $r29, 28
+addi $r30, $r30, 31
+addi $r31, $r31, 30
+
+stop:
+j stop
