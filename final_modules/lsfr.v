bc69e3921b5c8cec8ce1448c35642871088cb80d
wjn7 <wjn7@duke.edu>
2024-04-04 12:17:53 -0400

    fixed lsfr

--- final_modules/lsfr.v
+++ final_modules/lsfr.v
@@ -10,15 +10,19 @@ module lsfr(clock, reset, random_acc);
     
     // Initialize the LFSR register value upon startup
     initial begin
-        lfsr_reg = 32'b10101010101011010100100101001100;  // Set initial value to a specific value
+        lfsr_reg = 32'b10101010101011110110100101101100;  // Set initial value to a specific value
     end
 
     // Clock the LFSR
     always @(posedge clock or posedge reset) begin
         if (reset) begin
-            lfsr_reg <= 32'b10101010101011010100100101001100;  // Reset the LFSR to initial state (any non-zero value)
+            lfsr_reg <= 32'b10101010101011110110100101101100;  // Reset the LFSR to initial state (any non-zero value)
         end else begin
-            lfsr_reg <= {lfsr_reg[30:0], lfsr_reg[31]};  // Shift left
+            lfsr_reg[31] = lfsr_reg[30] ^ lfsr_reg[11]; // Feedback from taps 32 and 22
+            lfsr_reg[24] = lfsr_reg[0] ^ lfsr_reg[24];  // Feedback from taps 1 and 23
+            lfsr_reg[14] = lfsr_reg[8] ^ lfsr_reg[19];  // Feedback from taps 2 and 24
+            lfsr_reg[3] = lfsr_reg[5] ^ lfsr_reg[28];  // Feedback from taps 5 and 17
+            lfsr_reg = {lfsr_reg[30:0], lfsr_reg[31]};  // Shift left
         end
     end
 
