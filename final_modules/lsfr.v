de9d03786ff7b3d2b0e28fd7919708ee4e346055
wjn7 <wjn7@duke.edu>
2024-04-23 01:56:58 -0400

    small lfsr mods. reinstituted random

--- final_modules/lsfr.v
+++ final_modules/lsfr.v
@@ -14,7 +14,7 @@ module lsfr(clock, reset, randNum);
     // Initialize the LFSR register value upon startup
     initial begin
         lfsr_reg = 32'b10101110101011110110100101101100;  // Set initial value to a specific value
-        lfsr_reg1 = 32'b11011100011000111000001001000000;
+        lfsr_reg1 = 32'b10001111010100111101001000101001;
         lfsr_reg2 = 32'b01001101111001100011000100011110;
         lfsr_reg3 = 32'b11001001101100111010110100100100;
     end
@@ -23,7 +23,7 @@ module lsfr(clock, reset, randNum);
     always @(posedge clock or posedge reset) begin
         if (reset) begin
             lfsr_reg <= 32'b10001110101011110110100101101100;  // Reset the LFSR to initial state (any non-zero value)
-            lfsr_reg1 <= 32'b10001111010100111101000000101001;
+            lfsr_reg1 <= 32'b10001111010100111101001000101001;
             lfsr_reg2 <= 32'b01001101111001100011000100011110;
             lfsr_reg3 <= 32'b00110010010001100011011110001000;
         end else begin
@@ -34,8 +34,8 @@ module lsfr(clock, reset, randNum);
             // lfsr_reg[0] <= lfsr_reg[31] ^ lfsr_reg[29] ^ lfsr_reg[25] ^ lfsr_reg[24];
             
             lfsr_reg[0] <= lfsr_reg[1] ^ lfsr_reg[5] ^ lfsr_reg[6] ^ lfsr_reg[31]; //-> TO ADD
-            lfsr_reg1[0] <= lfsr_reg1[1] ^ lfsr_reg1[5] ^ lfsr_reg1[6] ^ lfsr_reg1[31];
-            lfsr_reg2[0] <= lfsr_reg2[1] ^ lfsr_reg2[5] ^ lfsr_reg2[6] ^ lfsr_reg2[31];
+            lfsr_reg1[0] <= lfsr_reg1[1] ^ lfsr_reg1[9] ^ lfsr_reg1[14] ^ lfsr_reg1[31];
+            lfsr_reg2[0] <= lfsr_reg2[2] ^ lfsr_reg2[5] ^ lfsr_reg2[6] ^ lfsr_reg2[31];
             lfsr_reg3[0] <= lfsr_reg3[1] ^ lfsr_reg3[5] ^ lfsr_reg3[6] ^ lfsr_reg3[31];
             
             lfsr_reg = lfsr_reg << 1;
@@ -64,8 +64,8 @@ module lsfr(clock, reset, randNum);
     assign rand[3] = lfsr_reg3[0]; 
 
     assign limiter = rand[3] & ~rand[2] & ~rand[1] & ~rand[0];
-    assign random_acc = 4'b1111;
-    // assign random_acc = limiter ?  4'd0 : rand; //want to keep + and - sides even, but 2's complement isn't even, so -8 becomes 0
+    // assign random_acc = 4'b1111;
+    assign random_acc = limiter ?  4'd0 : rand; //want to keep + and - sides even, but 2's complement isn't even, so -8 becomes 0
     // 4'b1001
     assign randNum[4] = random_acc[3];
     assign randNum[5] = random_acc[3];
